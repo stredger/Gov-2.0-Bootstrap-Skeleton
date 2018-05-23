e99e079dffaef100ca84074a1c10cbe708e49c43
Stephen Tredger <stredger@gmail.com>
2018-05-23 10:07:18 -0700

    add new css from tfrs mockups

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import './css/ie10-viewport-bug-workaround.css'
 import './css/custom-form.css'
 import './css/custom-main.css'
 import './css/main.css'
+import './css/tfrs-additional.css'
 
 // js to bundle
 import './js/bootstrap'
